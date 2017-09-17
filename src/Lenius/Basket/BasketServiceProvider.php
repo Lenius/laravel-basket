619a1a0e3e68facd6c7c979b18aeebd80835f7be
Carsten Jonstrup <carsten@lenius.dk>
2017-09-17 22:38:22 +0200

    Cleanup

--- src/Lenius/Basket/BasketServiceProvider.php
+++ src/Lenius/Basket/BasketServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
-* This file is part of Lenius Basket for Laravel 4, a PHP
+* This file is part of Lenius Basket for Laravel 5, a PHP
 * package to provide a Service Provider and Facade for
 * the Lenius\Basket package.
 *
-* Copyright (c) 2013 Lenius.
+* Copyright (c) 2017 Lenius.
 * http://github.com/lenius/laravel-basket
 *
 * For the full copyright and license information, please view the LICENSE
@@ -13,14 +13,15 @@
 *
 * @package lenius/laravel-basket
 * @author Carsten <carsten@lenius.dk>
-* @copyright 2013 Lenius.
-* @version dev
+* @copyright 2017 Lenius.
+* @version production
 * @link http://github.com/lenius/laravel-basket
 *
 */
 
 namespace Lenius\Basket;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 use Lenius\Basket\Storage\LaravelSession as SessionStore;
 use Lenius\Basket\Identifier\Cookie as CookieIdentifier;
@@ -36,7 +37,7 @@ class BasketServiceProvider extends ServiceProvider
         // Shortcut so developers don't need to add an Alias in app/config/app.php
         $this->app->booting(function()
         {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader = AliasLoader::getInstance();
             $loader->alias('Basket', 'Lenius\Basket\Facade');
         });
     }
--- src/Lenius/Basket/Facade.php
+++ src/Lenius/Basket/Facade.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
-* This file is part of Lenius Basket for Laravel 4, a PHP
+* This file is part of Lenius Basket for Laravel 5, a PHP
 * package to provide a Service Provider and Facade for
 * the Lenius\Basket package.
 *
-* Copyright (c) 2013 Lenius.
+* Copyright (c) 2017 Lenius.
 * http://github.com/lenius/laravel-basket
 *
 * For the full copyright and license information, please view the LICENSE
@@ -13,8 +13,8 @@
 *
 * @package lenius/laravel-basket
 * @author Carsten <carsten@lenius.dk>
-* @copyright 2013 Lenius.
-* @version dev
+* @copyright 2017 Lenius.
+* @version production
 * @link http://github.com/lenius/laravel-basket
 *
 */
